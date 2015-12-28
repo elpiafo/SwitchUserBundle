6a67d7dfa2159f956cfd52018d6fc23b284c7335
Adrien Saint-Vanne <adriensv@gmail.com>
2015-12-28 21:24:42 +0100

    Edit SwitchUser entity

--- Entity/SwitchUser.php
+++ Entity/SwitchUser.php
@@ -7,149 +7,86 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * Switch
  *
- * @ORM\Table(name="switch")
+ * @ORM\Table(name="switchuser")
  * @ORM\Entity(repositoryClass="Elpiafo\SwitchUserBundle\Repository\SwitchRepository")
  */
-class Switch
+class SwitchUser
 {
     /**
-     * @var int
-     *
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    protected $id;
 
     /**
-     * @var \stdClass
-     *
-     * @ORM\Column(name="grantor", type="object")
+     * Grantor user allows granted user
+     * @ORM\OneToOne(targetEntity="Symfony\Component\Security\Core\User\UserInterface", cascade={"remove"})
      */
-    private $grantor;
+    protected $grantor;
 
     /**
-     * @var \stdClass
-     *
-     * @ORM\Column(name="granted", type="object")
+     * Granted user is allowed by grantor user
+     * @ORM\OneToOne(targetEntity="Symfony\Component\Security\Core\User\UserInterface", cascade={"remove"})
      */
-    private $granted;
+    protected $granted;
 
     /**
-     * @var \DateTime
-     *
      * @ORM\Column(name="start_date", type="datetime", nullable=true)
      */
-    private $startDate;
+    protected $startDate;
 
     /**
-     * @var \DateTime
-     *
      * @ORM\Column(name="end_date", type="datetime", nullable=true)
      */
-    private $endDate;
+    protected $endDate;
 
     /**
-     * @var bool
-     *
      * @ORM\Column(name="active", type="boolean")
      */
-    private $active;
+    protected $active;
 
-
-    /**
-     * Get id
-     *
-     * @return int
-     */
     public function getId()
     {
         return $this->id;
     }
 
-    /**
-     * Set grantor
-     *
-     * @param \stdClass $grantor
-     *
-     * @return Switch
-     */
     public function setGrantor($grantor)
     {
         $this->grantor = $grantor;
-
         return $this;
     }
 
-    /**
-     * Get grantor
-     *
-     * @return \stdClass
-     */
     public function getGrantor()
     {
         return $this->grantor;
     }
 
-    /**
-     * Set granted
-     *
-     * @param \stdClass $granted
-     *
-     * @return Switch
-     */
     public function setGranted($granted)
     {
         $this->granted = $granted;
-
         return $this;
     }
 
-    /**
-     * Get granted
-     *
-     * @return \stdClass
-     */
     public function getGranted()
     {
         return $this->granted;
     }
 
-    /**
-     * Set startDate
-     *
-     * @param \DateTime $startDate
-     *
-     * @return Switch
-     */
     public function setStartDate($startDate)
     {
         $this->startDate = $startDate;
-
         return $this;
     }
 
-    /**
-     * Get startDate
-     *
-     * @return \DateTime
-     */
     public function getStartDate()
     {
         return $this->startDate;
     }
 
-    /**
-     * Set endDate
-     *
-     * @param \DateTime $endDate
-     *
-     * @return Switch
-     */
     public function setEndDate($endDate)
     {
         $this->endDate = $endDate;
-
         return $this;
     }
 
@@ -163,26 +100,13 @@ class Switch
         return $this->endDate;
     }
 
-    /**
-     * Set active
-     *
-     * @param boolean $active
-     *
-     * @return Switch
-     */
     public function setActive($active)
     {
         $this->active = $active;
-
         return $this;
     }
 
-    /**
-     * Get active
-     *
-     * @return bool
-     */
-    public function getActive()
+    public function isActive()
     {
         return $this->active;
     }
