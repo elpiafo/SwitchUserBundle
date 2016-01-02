4cf88effb4799d69255fb442771e73e4012f0253
Adrien Saint-Vanne <adriensv@gmail.com>
2016-01-02 13:35:56 +0100

    Add Switch controller

--- Controller/SwitchController.php
+++ Controller/SwitchController.php
@@ -1,7 +1,42 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Adrien
- * Date: 02/01/2016
- * Time: 10:46
- */
+
+namespace Elpiafo\SwitchUserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
+class SwitchController extends Controller
+{
+
+    public function switchAction($userId)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $currentUser = $this->getUser();
+        $grantorUser = $em->getRepository('Symfony\Component\Security\Core\User\UserInterface');
+        if (!$grantorUser) {
+            $this->createNotFoundException('Requested user not found');
+        }
+        if (!$em->getRepository('ElpiafoSwitchUserBundle:SwitchUser')->isAllowed($userId, $currentUser->getId())) {
+            $this->createAccessDeniedException('Requested user has not granted current account');
+        }
+        $this->switchUser($grantorUser, null, $this->get('security.token_storage')->getToken()->getProviderKey(), $grantorUser->getRoles());
+        $this->switchSuccessRedirect();
+    }
+
+    protected function switchSuccessRedirect()
+    {
+        $this->redirect('/');
+    }
+
+    protected function switchUser($user, $credentials, $providerKey, array $roles)
+    {
+        $token = new UsernamePasswordToken(
+            $user,
+            $credentials,
+            $providerKey,
+            $roles
+        );
+        $this->container->get('security.token_storage')->setToken($token);
+    }
+}
